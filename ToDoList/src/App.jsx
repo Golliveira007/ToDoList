9dc4896d3410a0e35b6a31398b83c67c49bab326
Gabriel Oliveira <gabriel.oliveira@yazo.com.br>
2025-06-10 21:34:12 -0300

    Introdução no projeto com os dados que serão ultilizados e adicionado o maps que irá percorrer toda a lista com os dados adicionados

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -2,12 +2,42 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Estudar",
+      category: "(Estudos)",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Trabalhar",
+      category: "(Trabalho)",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Programar",
+      category: "(Programação)",
+      isCompleted: false,
+    }
+  ]);
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>Já me livrei da maldição</p>
+    <div className='app'>
+      <h1>Lista de Tarefas</h1>
+      <div className='todo-list'>
+        {todos.map((todo) => (
+          <div className='todo'>
+            <div className='content'>
+              <p>{todo.text}</p>
+              <p className="category">
+                {todo.category}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
